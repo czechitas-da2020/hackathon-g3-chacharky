59f33e66fa3166b7d3f3a51a3919497814a14cd5
Vojtěch Červený <cervik49@gmail.com>
2021-05-18 19:28:06 +0200

    about me test with console.log

---
+++ test/specs/aboutme.spec.js
@@ -0,0 +1,13 @@
+const templatePage = require('../pages/template.page')
+
+describe('About me  page', () => {
+    it('template test-case', () => {
+        console.log("blablabalbla")
+    })
+})
+
+
+// git commit 
+// git push 
+
+// git pull
