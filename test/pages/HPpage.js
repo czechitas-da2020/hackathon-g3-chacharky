bd136f8729666d622235e87f16d7c28f5f67a54d
Martina Kucerova <kucema724@gmail.com>
2021-05-22 14:38:15 +0200

    added the comments to the test and small changer in test for URL

--- test/pages/HPpage.js
+++ test/pages/HPpage.js
@@ -1,10 +1,11 @@
 let homePage = {
-    get header() {return $('#header')},
-    get searchForm() {return $('.header-rmsearch-primary')},
-    get interiorSection() {return $('div#hotelInteriorBlock')},
-    get amenitiesSection() {return $('div#hotelAmenitiesBlock')},
-    get ourRoomsSection() {return $('div#hotelRoomsBlock')},
-    get guestReviewSection() {return $('div#hotelTestimonialBlock')},
-    get footer() {return $('.footer-container')}
+    get hotelName() {return $('.header-hotel-name')}, // refers to the name of hotel on main page
+    get header() {return $('#header')}, //refers to the part of page - header
+    get searchForm() {return $('.header-rmsearch-primary')}, //refers to the part of page - search form
+    get interiorSection() {return $('div#hotelInteriorBlock')}, //refers to the part of page - interior section
+    get amenitiesSection() {return $('div#hotelAmenitiesBlock')}, //refers to the part of page - amenities section
+    get ourRoomsSection() {return $('div#hotelRoomsBlock')}, //refers to the part of page - our room section
+    get guestReviewSection() {return $('div#hotelTestimonialBlock')}, //refers to the part of page - review of the host section
+    get footer() {return $('.footer-container')} //refers to the part of page - footer
 }
 module.exports = homePage
--- test/specs/homePage.js
+++ test/specs/homePage.js
@@ -5,41 +5,43 @@ describe('Homepage', () => {
         browser.url('./')
     })
 
-    /* it('in english is displayed after enters URL of the website for english mutation', () => {
+    
+    it('for english mutation have a right URL', () => {
         browser.url('./en/');
-        expect(browser).toHaveUrlContaining("czechitas-hackhaton.cz/en")
+        expect(browser).toHaveUrlContaining("czechitas-hackhaton.cz/en") // check if the URL meet the requirment
     })
 
-    it('in czech is displayed after enters URL of the website for czech mutation',()=>{
+    it('for czech mutation have a right URL',()=>{
         browser.url('./cs/');
-        expect(browser).toHaveUrlContaining('czechitas-hackhaton.cz/cs')
-    }) */
+        expect(browser).toHaveUrlContaining('czechitas-hackhaton.cz/cs') // check if the URL meet the requirment
+    })
 
+    
     it('contains header', ()=>{
-        expect(homePage.header).toBeDisplayed();
+        expect(homePage.header).toBeDisplayed(); // check if the homepage contains header or not
     })
 
     it('contains search form', ()=>{
-        expect(homePage.searchForm).toBeDisplayed();
+        expect(homePage.searchForm).toBeDisplayed(); // check if the homepage contains search form or not
     })
 
     it('contains interior section', ()=>{
-        expect(homePage.interiorSection).toBeDisplayed();
+        expect(homePage.interiorSection).toBeDisplayed(); // check if the homepage contains interior section or not
     })
 
     it('contains amenities section', ()=>{
-        expect(homePage.amenitiesSection).toBeDisplayed();
+        expect(homePage.amenitiesSection).toBeDisplayed(); // check if the homepage contains amenities section or not
     })
 
     it('contains our rooms section', ()=>{
-        expect(homePage.ourRoomsSection).toBeDisplayed();
+        expect(homePage.ourRoomsSection).toBeDisplayed(); // check if the homepage contains our rooms or not
     })
 
     it('contains customer review section', ()=>{
-        expect(homePage.guestReviewSection).toBeDisplayed();
+        expect(homePage.guestReviewSection).toBeDisplayed(); // check if the homepage contains customer review or not
     })
 
     it('contains footer', () =>{
-        expect(homePage.footer).toBeDisplayed();
+        expect(homePage.footer).toBeDisplayed(); // check if the homepage contains footer or not
     })
 })
